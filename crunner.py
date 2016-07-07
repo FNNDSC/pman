e0ece168e466abab2abb9fea4f0fc6fe34ccff28
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-07-07 11:05:08 -0400

    Fix typo.

--- crunner.py
+++ crunner.py
@@ -672,8 +672,8 @@ class crunner(object):
             # This short timeout during the very last job loop is needed for
             # synchronization at the very end of the processing loop. It's
             # probably a very very BAD idea to do this but I couldn't figure
-            # out another way. Each loop needed a small delay at the start so
-            # that the final all-loops-done event can sync properly.
+            # out another way. The final loop needed a small delay at the start
+            # so that the final all-loops-done event can sync properly.
             if self.jobCount == self.jobTotal-1: time.sleep(timeout)
 
             # Wait for the start event trigger
