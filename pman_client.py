88a1c60da195237c716e1541a4ea50dd9d449a25
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-07-25 18:32:06 -0400

    Add first test suite.

--- pman_client.py
+++ pman_client.py
@@ -1,32 +1,58 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.5
 
 '''
 
 Send simple messages to pman server.
 
 '''
-# Author - Kasun Herath <kasunh01 at gmail.com>
-# Source - https://github.com/kasun/zeromq-client-server.git
 
 
-from __future__ import print_function
-
-import  threading
-import  zmq
+# import  threading
 import  argparse
 from    _colors     import  Colors
+import  crunner
+import  time
+import  sys
+import  json
+import  pprint
 
-class Client(threading.Thread):
+class Client():
+
     ''' Represents an example client. '''
-    def __init__(self, **kwargs):
-        threading.Thread.__init__(self)
 
-        for key,val in kwargs.iteritems():
-            if key == 'id':         self.identity       = val
+    def __init__(self, **kwargs):
+        # threading.Thread.__init__(self)
+
+        self.str_cmd        = ""
+        self.str_ip         = ""
+        self.str_port       = ""
+        self.str_msg        = ""
+        self.str_testsuite  = ""
+        self.str_protocol   = "http"
+        self.str_auid       = "<auid>"
+        self.str_jid        = "<jid>"
+        self.loopStart      = 0
+        self.loopEnd        = 0
+        self.txpause        = 1
+        self.pp             = pprint.PrettyPrinter(indent=4)
+
+        self.shell                      = crunner.crunner()
+        self.shell.b_splitCompound      = True
+        self.shell.b_showStdOut         = True
+        self.shell.b_showStdErr         = True
+        self.shell.b_echoCmd            = True
+
+        for key,val in kwargs.items():
+            if key == 'cmd':        self.str_cmd        = val
             if key == 'msg':        self.str_msg        = val
-            if key == 'protocol':   self.str_protocol   = val
-            if key == 'ip':         self.str_IP         = val
+            if key == 'ip':         self.str_ip         = val
             if key == 'port':       self.str_port       = val
+            if key == 'auid':       self.str_auid       = val
+            if key == 'jid':        self.str_jid        = val
+            if key == 'txpause':    self.txpause        = int(val)
+            if key == 'testsuite':  self.str_testsuite  = val
+            if key == 'loopStart':  self.loopStart      = int(val)
+            if key == 'loopEnd':    self.loopEnd        = int(val)
 
         print(Colors.LIGHT_GREEN)
         print("""
@@ -35,74 +61,97 @@ class Client(threading.Thread):
         \t+---------------------------------+
         """)
         print(Colors.CYAN + """
-        This program sends MSG payloads to a 'pman' listener
+        This program sends command payloads to a 'pman' process manager. A
+        command is a typical bash command string to be executed and managed
+        by pman.
+
+        In addition, several control messages can also be sent to 'pman'. Typically
+        these relate to the saving of the internal database.
+
+        Several "canned" command payloads are available, and are triggered
+        by an approprite "--testsuite <index> [--loop <loop>]":
+
+            --testsuite 1:
+            Create <loop> instances of 'cmd'. These are in fact 'eval'ed, so
+            C-style literals like '%d' are replaced by the current <loop>.
+
+            --testsuite 2:
+            Get first <loop> jobInfo packages.
         """)
 
         print(Colors.WHITE + "\t\tWill transmit to: " + Colors.LIGHT_BLUE, end='')
-        print('%s://%s:%s' % (self.str_protocol, self.str_IP, self.str_port))
+        print('%s://%s:%s' % (self.str_protocol, self.str_ip, self.str_port))
+        print(Colors.WHITE + "\t\tIter-transmit delay: " + Colors.LIGHT_BLUE, end='')
+        print('%d second(s)' % (self.txpause))
 
-        self.zmq_context = zmq.Context()
+    def testsuite_handle(self, **kwargs):
+        """
+        Handle the test suites.
+        """
+
+        # First, process the <cmd> string for any special characters
+
+        testsuite       = 0
+
+        str_shellCmd    = ""
+        str_cmd = self.str_cmd
+
+        for k,v in kwargs.items():
+            if k == 'testsuite':    testsuite   = int(v)
+
+        for l in range(self.loopStart, self.loopEnd):
+
+            if testsuite    == 0:
+                if "%d" in self.str_cmd:
+                    str_cmd = self.str_cmd.replace("%d", str(l))
+                    # print(str_cmd)
+                str_shellCmd    = "http POST http://%s:%s/api/v1/cmd/ Content-Type:application/json Accept:application/json payload:='{\"exec\": {\"cmd\": \"%s\"}, \"action\":\"run\",\"meta\":{\"auid\":\"%s\", \"jid\":\"%s-%d\"}}'" \
+                                        % (self.str_ip, self.str_port, str_cmd, self.str_auid, self.str_jid, l)
+                self.shell                      = crunner.crunner(verbosity=-1)
+                self.shell.b_splitCompound      = True
+                self.shell.b_showStdOut         = True
+                self.shell.b_showStdErr         = True
+                self.shell.b_echoCmd            = True
+                print(Colors.LIGHT_BLUE)
+                print("Sending...")
+                self.shell(str_shellCmd)
+                self.shell.jobs_loopctl()
+                print(Colors.LIGHT_GREEN)
+                print("Receiving...")
+                json_stdout  = json.loads(self.shell.d_job["0"]["stdout"])
+                self.pp.pprint(json_stdout)
+                print("\n")
+                print("Pausing for %d second(s)..." % self.txpause)
+                time.sleep(self.txpause)
+                print("\n")
 
     def run(self):
         ''' Connects to server. Send message, poll for and print result to standard out. '''
-        print(Colors.LIGHT_PURPLE + 'Client ID - %s. msg to transmit - %s ' % (self.identity, self.str_msg))
-        socket = self.connection_get()
-        
-        # Poller is used to check for availability of data before reading from a socket.
-        poller = zmq.Poller()
-        poller.register(socket, zmq.POLLIN)
-        self.send(socket, '%s' % (self.str_msg))
 
-        # Infinitely poll for the result. 
-        # Polling is used to check for sockets with data before reading because socket.recv() is blocking.
-        while True:
-            # Poll for 5 seconds. Return any sockets with data to be read.
-            sockets = dict(poller.poll(5000))
+        if len(self.str_testsuite):
+            self.testsuite_handle()
 
-            # If socket has data to be read.
-            if socket in sockets and sockets[socket] == zmq.POLLIN:
-                result = self.receive(socket)
-                print('Client ID - %s. Received result - %s.' % (self.identity, result))
-                break
+        print(Colors.LIGHT_PURPLE + 'msg to transmit - %s ' % (self.str_msg))
 
-        socket.close()
-        self.zmq_context.term()
 
-    def send(self, socket, data):
-        ''' Send data through provided socket. '''
-        socket.send(data)
-
-    def receive(self, socket):
-        ''' Recieve and return data through provided socket. '''
-        return socket.recv()
-
-    def connection_get(self):
-        ''' Create a zeromq socket of type DEALER; set it's identity, connect to server and return socket. '''
-
-        # Socket type DEALER is used in asynchronous request/reply patterns.
-        # It prepends identity of the socket with each message.
-        socket = self.zmq_context.socket(zmq.DEALER)
-        socket.setsockopt(zmq.IDENTITY, self.identity)
-        socket.connect('%s://%s:%s' % (self.str_protocol, self.str_IP, self.str_port))
-        return socket
 
 if __name__ == '__main__':
 
     parser  = argparse.ArgumentParser(description = 'simple client for talking to pman')
 
     parser.add_argument(
+        '--cmd',
+        action  = 'store',
+        dest    = 'cmd',
+        default = '',
+        help    = 'Command to be managed by pman.'
+    )
+    parser.add_argument(
         '--msg',
         action  = 'store',
         dest    = 'msg',
         default = '',
-        help    = 'Message to send to pman instance.'
-    )
-    parser.add_argument(
-        '--id',
-        action  = 'store',
-        dest    = 'id',
-        default = "1",
-        help    = 'ID of client.'
+        help    = 'Control signal to send to pman.'
     )
     parser.add_argument(
         '--ip',
@@ -119,20 +168,60 @@ if __name__ == '__main__':
         help    = 'Port to use.'
     )
     parser.add_argument(
-        '--protocol',
+        '--jid',
+        help    = 'job ID.',
+        dest    = 'jid',
         action  = 'store',
-        dest    = 'protocol',
-        default = 'tcp',
-        help    = 'Protocol to use.'
+        default = '<jid>'
+    )
+    parser.add_argument(
+        '--auid',
+        help    = 'apparent user id',
+        dest    = 'auid',
+        action  = 'store',
+        default = '<aiud>'
+    )
+    parser.add_argument(
+        '--txpause',
+        help    = 'pause length between transmits',
+        dest    = 'txpause',
+        action  = 'store',
+        default = '1'
+    )
+    parser.add_argument(
+        '--testsuite',
+        help    = 'internal test suite',
+        dest    = 'testsuite',
+        action  = 'store',
+        default = ''
+    )
+    parser.add_argument(
+        '--loopStart',
+        help    = 'start internal testsuite loop at loopStart',
+        dest    = 'loopStart',
+        action  = 'store',
+        default = ''
+    )
+    parser.add_argument(
+        '--loopEnd',
+        help    = 'end internal testsuite loop at loopEnd',
+        dest    = 'loopEnd',
+        action  = 'store',
+        default = ''
     )
 
     args    = parser.parse_args()
     client  = Client(
-                        id          = args.id,
                         msg         = args.msg,
-                        protocol    = args.protocol,
+                        cmd         = args.cmd,
                         ip          = args.ip,
-                        port        = args.port
+                        port        = args.port,
+                        jid         = args.jid,
+                        auid        = args.auid,
+                        txpause     = args.txpause,
+                        testsuite   = args.testsuite,
+                        loopStart   = args.loopStart,
+                        loopEnd     = args.loopEnd
                 )
-    client.start()
-
+    client.run()
+    sys.exit(0)
