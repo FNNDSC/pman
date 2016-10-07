7d91f2e8ba75eb46f18899c6d3855e7a0d73fae3
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-10-07 14:43:35 -0400

    Add for pip install.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name          =   'pman',
+      version       =   '0.1',
+      description   =   '(Python) Process Manager',
+      author        =   'Rudolph Pienaar',
+      author_email  =   'rudolph.pienaar@gmail.com',
+      url           =   'https://github.com/FNNDSC/pman',
+      packages      =   ['pman', 'purl', 'crunner'],
+     )
