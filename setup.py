124f2648e3baaf47935f0c29e13e218796436d16
Nicolas Rannou <npm.rannou@gmail.com>
2016-10-11 11:40:17 +0200

    Update setup.py
    Closes #1

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setup(
       url           =   'https://github.com/FNNDSC/pman',
       packages      =   ['pman'],
       package_dir   =   {'pman': 'pman'},
+      install_requires=[ 'pycurl', ],
       license       =   'MIT'
      )
