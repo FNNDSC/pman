609e5797100ce08fab158836bd104abd1a732912
Nicolas Rannou <npm.rannou@gmail.com>
2016-10-11 11:41:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(
-      name          =   'pman',
-      version       =   '0.1',
-      description   =   '(Python) Process Manager',
-      author        =   'Rudolph Pienaar',
-      author_email  =   'rudolph.pienaar@gmail.com',
-      url           =   'https://github.com/FNNDSC/pman',
-      packages      =   ['pman'],
-      package_dir   =   {'pman': 'pman'},
-      install_requires=[ 'pycurl', ],
-      license       =   'MIT'
+      name             =   'pman',
+      version          =   '0.1',
+      description      =   '(Python) Process Manager',
+      author           =   'Rudolph Pienaar',
+      author_email     =   'rudolph.pienaar@gmail.com',
+      url              =   'https://github.com/FNNDSC/pman',
+      packages         =   ['pman'],
+      package_dir      =   {'pman': 'pman'},
+      install_requires =   ['pycurl'],
+      license          =   'MIT'
      )
