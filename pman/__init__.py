d3f5263d6141d31041f34c7af3dee8aaf5b45f5d
nicolasrannou <npm.rannou@gmail.com>
2016-10-31 14:31:12 +0100

    add message tests

--- pman/__init__.py
+++ pman/__init__.py
@@ -1,2 +1,5 @@
-from .pman   import pman
-from .purl   import Purl
+from ._colors import Colors
+from .crunner import crunner
+from .message import Message
+from .pman    import pman
+from .purl    import Purl
