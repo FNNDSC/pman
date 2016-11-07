3f6b445dfb07d947827e978a9c7ff4071b0c6fc7
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2016-11-07 11:24:26 -0500

    Update to 0.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '0.9.1',
+      version          =   '0.10',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
