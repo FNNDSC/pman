26ea7f4c241c6f3fc0a4815a72db1126c3c4b0aa
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-03-10 17:25:46 -0500

    Update tags to 0.11.0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '0.10.4',
+      version          =   '0.11.0',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
