533dc97e8ed3a2ef8f002a6cd2a861a868b3f116
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-05-17 17:12:12 -0400

    Remove unnecessary imports.

--- pman/__init__.py
+++ pman/__init__.py
@@ -3,6 +3,6 @@ from .C_snode import C_stree
 from .crunner import crunner, crunner_ssh
 from .debug   import debug
 from .message import Message
-from .pfioh   import StoreHandler, ThreadedHTTPServer
+#from .pfioh   import StoreHandler, ThreadedHTTPServer
 from .pman    import pman
-from .purl    import Purl
+#from .purl    import Purl
