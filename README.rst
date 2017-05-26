981ad0b71d9064e116edbfd244815b46ace01256
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-05-26 15:00:16 -0400

    Update to 1.1.0 with minor bump due to swarm integration.

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ##############
-pman - v1.0.7
+pman - v1.1.0
 ##############
 
 .. image:: https://badge.fury.io/py/pman.svg
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '1.0.7',
+      version          =   '1.1.0',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
