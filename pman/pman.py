32ed3886102dffc0884683c50b2e208793c9b137
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-07-14 15:08:33 -0400

    Add inet addr to sysinfo hello.

--- pman/pman.py
+++ pman/pman.py
@@ -1147,6 +1147,7 @@ class Listener(threading.Thread):
                 d_ret['sysinfo']['loadavg']     = os.getloadavg()
                 d_ret['sysinfo']['cpu_percent'] = psutil.cpu_percent()
                 d_ret['sysinfo']['hostname']    = socket.gethostname()
+                d_ret['sysinfo']['inet']        = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
                 b_status                        = True
             if str_askAbout == 'echoBack':
                 d_ret['echoBack']               = {}
