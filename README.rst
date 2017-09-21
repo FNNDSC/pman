8931aff43b68ad13129b856fa1eecd93b2d24482
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-09-21 17:05:52 -0400

    Update to 1.4.3

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ##############
-pman - v1.4.2
+pman - v1.4.3
 ##############
 
 .. image:: https://badge.fury.io/py/pman.svg
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '1.4.2',
+      version          =   '1.4.3',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
