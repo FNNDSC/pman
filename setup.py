0b84e30313bb487d7e660e03d58b59fc1ab39ab8
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-02-13 14:37:21 -0500

    Bump for removing spurious pudbs.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '1.6.16.2',
+      version          =   '1.6.16.4',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
