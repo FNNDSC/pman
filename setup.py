795daac20d9128b4bb3616461c2d6367353683da
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-02-14 16:14:30 -0500

    Bump version up to reflect latest internals.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '1.6.16.4',
+      version          =   '1.6.16.6',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
