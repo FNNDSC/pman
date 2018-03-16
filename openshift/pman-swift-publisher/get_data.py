1ae85b92ffd99b048b812f65ba8971e2e6538ccf
umohnani8 <umohnani@redhat.com>
2018-03-16 11:38:28 -0400

    Scale how the data is downloaded in get_data.py
    If there are more than 1 pod, the init-container of one pod should download
    the data while the init-containers of the other pods wait. Once the data is downloaded
    the processing container of all the pods can access the data.
    
    Update pman and job template
    Change the memory value to 150Mi as that is the minimum allowed on MOC.
    Change the volume type for the shared directory to be a persistent volume claim
    with the name of the claim being `job_id`-storage-claim.
    
    Add function to create and delete PVC for job
    Creates a Persistent Volume Claim with the name "job-id"-storage-claim
    when an OpenShift job is created.
    Deletes the created PVC when the job is shut down.
    
    Remove hardcoding for incoming and outgoing directories
    Pick up the paths of the incoming and outgoing directories from
    the cmd.
    
    Signed-off-by: umohnani8 <umohnani@redhat.com>

--- openshift/pman-swift-publisher/get_data.py
+++ openshift/pman-swift-publisher/get_data.py
@@ -5,6 +5,8 @@ SWIFT_KEY enviornment variable to be passed by the template
 
 import os
 import zipfile
+import time
+import fasteners
 from io import BytesIO
 from swift_handler import SwiftHandler
 
@@ -40,7 +42,12 @@ class SwiftStore():
         """
 
         for k,v in kwargs.items():
-            if k== 'path': key= v
+            if k == 'path':
+                key = v
+            if k == 'in_dir':
+                incoming_dir = v
+            if k == 'out_dir':
+                outgoing_dir = v
 
         try:
             swiftHandler = SwiftHandler()
@@ -56,17 +63,36 @@ class SwiftStore():
         fileBytes  = BytesIO(fileContent)
 
         zipfileObj = zipfile.ZipFile(fileBytes, 'r', compression = zipfile.ZIP_DEFLATED)
-        # We are extracting to the file to /share/incoming in container as plugin container is hardcoded to read from
-        # /share/incoming.
-        # TODO: @ravig. Remove this hardcoding. Need to have named arguments in all plugins.
-        zipfileObj.extractall('/share/incoming')
-        # Create /share/outgoing directory
-        if not os.path.exists('/share/outgoing'):
-            os.makedirs('/share/outgoing')
-
-        
+        # We are extracting to the file to incoming_dir in container
+        zipfileObj.extractall(incoming_dir)
+        # Create outgoing_dir directory as the plugin container will output data there after processing.
+        if not os.path.exists(outgoing_dir):
+            os.makedirs(outgoing_dir)
 
 if __name__ == "__main__":
-
+    incoming_dir = os.environ.get("INCOMING_DIR")
     obj = SwiftStore()
-    obj.getData(path= os.environ.get('SWIFT_KEY'))
+    # The init-storage container in all the pods should acquire the lock
+    with fasteners.InterProcessLock("/share/.lockfile"):
+        # If "/share/.download-failed" exists, exit with an error code immediately
+        if os.path.exists("/share/.download-failed"):
+            print("Previous pod failed to download the data. Exiting with failure...")
+            exit(1)
+        # If there is some data in incoming_dir but "/share/.download-succeeded" doesn't exist, it is a failure case
+        # Exit with error code immediately
+        if os.path.exists(incoming_dir) and len(os.listdir(incoming_dir)) > 0 and not os.path.exists('/share/.download-succeeded'):
+            print("Some data was downloaded, but '/share/.download-succeeded' file doen't exist. Exiting with failure...")
+            exit(1)
+        # Download the data if "/share/.download-succeeded" does not exist
+        if not os.path.exists('/share/.download-succeeded'):
+            try:
+                print("Lock acquired. Downloading data from Swift...")
+                obj.getData(path=os.environ.get('SWIFT_KEY'), in_dir=incoming_dir, out_dir=os.environ.get('OUTGOING_DIR'))
+            except Exception as err:
+                print("Failed to download the data:", err)
+                # Create a failed file, if download failed to complete
+                os.mknod("/share/.download-failed")
+                exit(1)
+            # Create a success file, if download completed successfully
+            os.mknod("/share/.download-succeeded")
+    print("Data downloaded!")
