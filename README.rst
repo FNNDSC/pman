1c226bcf8269d190c0b9aa98d0a6fad77d1fea61
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-09-10 16:28:27 -0400

    Bump to 2.0.0.4

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 #################
-pman - v2.0.0.2
+pman - v2.0.0.4
 #################
 
 .. image:: https://badge.fury.io/py/pman.svg
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
       name             =   'pman',
-      version          =   '2.0.0.2',
+      version          =   '2.0.0.4',
       description      =   '(Python) Process Manager',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
