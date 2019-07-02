6086bed8b96e5ad5ba1d594126ac375d332e8a14
Arnav N <arnavn@localhost.localdomain>
2019-07-02 09:32:04 -0400

    Added MOC framework

--- openshift/pman-health-check/setup.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-subprocess.call("pip3 install pfurl", shell=True)
-
-try:
-	subprocess.call("apt-get install -y libssl-dev libcurl4-openssl-dev bsdmainutils vim net-tools inetutils-ping")
-except Exception:
-	pass
-
