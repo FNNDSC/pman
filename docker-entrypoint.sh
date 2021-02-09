040450c4ff9c1bb354bf865fc9b82e1db8a64fa6
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-08 20:59:15 -0500

    Chgrp STOREBASE to gid=0

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,8 +16,25 @@ print(v.attrs['Mountpoint'])
 
 if [ ! -v STOREBASE ] && [ -n "$PMAN_DOCKER_VOLUME" ]; then
   if mountpoint=$(python -c "$get_volume_mountpoint_py"); then
-    export STOREBASE=$mountpoint
+    export STOREBASE="$mountpoint"
   fi
+fi
+
+# While STOREBASE is a workaround for working with the swarm scheduler,
+# we can borrow some advice from OpenShift.
+#
+# The user of a container is always a member of the group with GID=0
+#
+# By granting the root (GID=0) group rw (read-write) permission to STOREBASE,
+# we can run ChRIS plugin containers as underprivileged users with arbitrary UID
+# and still allow them to write to the shared directories (volume mounts).
+#
+# https://www.openshift.com/blog/a-guide-to-openshift-and-uids
+# https://docs.openshift.com/container-platform/3.3/creating_images/guidelines.html#openshift-container-platform-specific-guidelines
+
+if [ -d "$STOREBASE" ]; then
+  chgrp -R 0 "$STOREBASE"
+  chmod -R g+rw "$STOREBASE"
 fi
 
 exec pman $@
