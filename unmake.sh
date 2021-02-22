9f2003876d923b4fbce0722757a17e3753741af1
Jorge <jbernal0019@yahoo.es>
2021-02-22 17:32:51 -0500

    Improve unmake.sh script

--- unmake.sh
+++ unmake.sh
@@ -6,30 +6,12 @@ declare -i STEP=0
 
 export STOREBASE=${STOREBASE}
 
-title -d 1 "Destroying containerized development environment" "from  ./docker-compose_dev.yml"
-    echo "Do you want to also remove persistent volumes? [y/n]"             | ./boxes.sh
-    windowBottom
-    old_stty_cfg=$(stty -g)
-    stty raw -echo ; REPLY=$(head -c 1) ; stty $old_stty_cfg
-    echo -en "\033[2A\033[2K"
-    # read -p  " " -n 1 -r REPLY
-    if [[ $REPLY =~ ^[Yy]$ ]] ; then
-        printf "Removing persistent volumes...\n"                           | ./boxes.sh ${Yellow}
-        echo "This might take a few minutes... please be patient."          | ./boxes.sh ${Yellow}
-        windowBottom
-        docker-compose --no-ansi -f docker-compose_dev.yml down -v >& dc.out >/dev/null
-        echo -en "\033[2A\033[2K"
-        cat dc.out | ./boxes.sh
-        echo "Removing ./FS tree"                                           | ./boxes.sh
-        rm -fr ./FS
-    else
-        printf "Keeping persistent volumes...\n"                            | ./boxes.sh ${Yellow}
-        echo "This might take a few minutes... please be patient."          | ./boxes.sh ${Yellow}
-        windowBottom
-        docker-compose --no-ansi -f docker-compose_dev.yml down >& dc.out >/dev/null
-        echo -en "\033[2A\033[2K"
-        cat dc.out | ./boxes.sh
-    fi
+title -d 1 "Destroying pman containerized development environment" \
+                    "from ./docker-compose_dev.yml..."
+    docker-compose -f docker-compose_dev.yml --no-ansi down >& dc.out >/dev/null
+    cat dc.out                                                              | ./boxes.sh
+    echo "Removing ./FS tree"                                               | ./boxes.sh
+    rm -fr ./FS
 windowBottom
 
 title -d 1 "Stopping swarm cluster..."
