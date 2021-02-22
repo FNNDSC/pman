8138b553bd9ff0f8c7acd4028e79c4a529d48479
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-22 17:37:01 -0500

    Backwards compatibility with old dockerd

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
 
 setup(
     name             =   'pman',
-    version          =   '3.1.0',
+    version          =   '3.1.0-rc.2',
     description      =   'Process Manager',
     long_description =   readme,
     author           =   'FNNDSC Developers',
